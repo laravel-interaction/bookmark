cbee95ccd9874adfeeb4f939b31d09a46a295b89
Zing <zingimmick@outlook.com>
2021-04-18 17:27:48 +0800

    Return the last entry when reaction created. (#24)
    * Return the last entry when raction created.
    
    * Rector Rectify
    
    * Rector Rectify
    
    * Return the last entry when raction created.
    
    * Rector Rectify
    
    * Return the last entry when raction created.
    
    * Rector Rectify
    
    * Return the last entry when raction created.
    
    * Rector Rectify
    
    Co-authored-by: zingimmick <zingimmick@users.noreply.github.com>

--- src/Concerns/Bookmarker.php
+++ src/Concerns/Bookmarker.php
@@ -7,6 +7,7 @@ namespace LaravelInteraction\Bookmark\Concerns;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use LaravelInteraction\Bookmark\Bookmark;
 
 /**
  * @property-read \Illuminate\Database\Eloquent\Collection|\LaravelInteraction\Bookmark\Bookmark[] $bookmarkerBookmarks
@@ -16,16 +17,27 @@ trait Bookmarker
 {
     /**
      * @param \Illuminate\Database\Eloquent\Model $object
+     *
+     * @return \LaravelInteraction\Bookmark\Bookmark
      */
-    public function bookmark(Model $object): void
+    public function bookmark(Model $object): Bookmark
     {
-        $hasBookmarked = $this->hasBookmarked($object);
-        if ($hasBookmarked) {
-            return;
-        }
+        $attributes = [
+            'bookmarkable_id' => $object->getKey(),
+            'bookmarkable_type' => $object->getMorphClass(),
+        ];
 
-        $this->bookmarks(get_class($object))
-            ->attach($object->getKey());
+        return $this->bookmarkerBookmarks()
+            ->where($attributes)
+            ->firstOr(function () use ($attributes) {
+                $bookmarkerBookmarksLoaded = $this->relationLoaded('bookmarkerBookmarks');
+                if ($bookmarkerBookmarksLoaded) {
+                    $this->unsetRelation('bookmarkerBookmarks');
+                }
+
+                return $this->bookmarkerBookmarks()
+                    ->create($attributes);
+            });
     }
 
     /**
@@ -62,24 +74,31 @@ trait Bookmarker
 
     /**
      * @param \Illuminate\Database\Eloquent\Model $object
+     *
+     * @return bool|\LaravelInteraction\Bookmark\Bookmark
      */
-    public function toggleBookmark(Model $object): void
+    public function toggleBookmark(Model $object)
     {
-        $this->bookmarks(get_class($object))
-            ->toggle($object->getKey());
+        return $this->hasBookmarked($object) ? $this->unbookmark($object) : $this->bookmark($object);
     }
 
     /**
      * @param \Illuminate\Database\Eloquent\Model $object
+     *
+     * @return bool
      */
-    public function unbookmark(Model $object): void
+    public function unbookmark(Model $object): bool
     {
         $hasNotBookmarked = $this->hasNotBookmarked($object);
         if ($hasNotBookmarked) {
-            return;
+            return true;
+        }
+        $bookmarkerBookmarksLoaded = $this->relationLoaded('bookmarkerBookmarks');
+        if ($bookmarkerBookmarksLoaded) {
+            $this->unsetRelation('bookmarkerBookmarks');
         }
 
-        $this->bookmarks(get_class($object))
+        return (bool) $this->bookmarks(get_class($object))
             ->detach($object->getKey());
     }
 
