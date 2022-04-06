bb3a41c3c1d8ab23a2e27e8d4f26a5c2a62829bf
Zing <zingimmick@outlook.com>
2022-04-06 20:16:14 +0800

    Add type hint for constants (#119)

--- src/Concerns/Bookmarkable.php
+++ src/Concerns/Bookmarkable.php
@@ -49,6 +49,8 @@ trait Bookmarkable
     }
 
     /**
+     * @phpstan-param 1|2|3|4 $mode
+     *
      * @param array<int, string>|null $divisors
      */
     public function bookmarkersCountForHumans(
