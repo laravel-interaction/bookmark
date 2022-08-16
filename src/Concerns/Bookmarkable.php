abfc669cea66e048371ac55d5e25fd82dbbdef48
Zing <zingimmick@outlook.com>
2022-08-16 20:35:20 +0800

    Fix coding style (#130)

--- src/Concerns/Bookmarkable.php
+++ src/Concerns/Bookmarkable.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use LaravelInteraction\Support\Interaction;
+
 use function is_a;
 
 /**
